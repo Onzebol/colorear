0e47eaa5844f8e1b96a77ff82e7d9c2c933ff771
Luis Miguel <lmaimr@hotmail.com>
2022-09-25 11:16:03 +0200

    Cambioos para que funcionen las imagenes en GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://onzebol.github.io/colorear',
+  site: 'https://onzebol.github.io',
   base: '/colorear'
 });
